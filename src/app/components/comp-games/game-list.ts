4233640e9aa88b68922a5ae53f62dbea2e9ca217
andrew hayes <ahayes5@oswego.edu>
2020-06-19 14:47:56 -0400

    Added rocket league to game list

--- src/app/components/comp-games/game-list.ts
+++ src/app/components/comp-games/game-list.ts
@@ -3,5 +3,6 @@ import {Game} from './game';
 export const GAMES: Game[] = [
   {name: 'Rainbow Six Siege', link: '/comp-games/siege', image: 'assets/siegeIcon.png'},
   {name: 'League Of Legends', link: '/comp-games/league', image: 'assets/leagueIcon.png'},
-  {name: 'Smash', link: '/comp-games/smash',  image: 'assets/smashIcon.png'}
+  {name: 'Smash', link: '/comp-games/smash',  image: 'assets/smashIcon.png'},
+  {name: 'Rocket League', link: '/comp-games/rocketLeague', image: 'assets/rocketLeagueIcon.png'}
 ];
